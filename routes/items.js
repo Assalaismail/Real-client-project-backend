2ef00665f92424b76d0b2a939e7b6df3e83f5a68
Assala <ismailassala1@gmail.com>
2023-04-20 08:56:55 +0300

    discounts

--- routes/items.js
+++ routes/items.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const upload= require('../middleware/upload')
 
-const {getitembyid,getitems,getItemsByCategory, getItemsByCategoryName, postitems,deleteitems, updateitems}=require("../controllers/items");
+const {getitembyid,getitems,getdiscount,getItemsByCategory, getItemsByCategoryName, postitems,deleteitems, updateitems}=require("../controllers/items");
 
 router.get("/getitem",getitems)
 router.get("/getitem/:id", getitembyid);
 router.get("/items/:category_id", getItemsByCategory );
 router.get("/products/:categoryName", getItemsByCategoryName );
+router.get("/getdiscount",getdiscount)
+
 
 
 router.post("/additem",upload.single('image'),postitems)
