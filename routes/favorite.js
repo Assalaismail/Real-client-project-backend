0927500db6dbb6b8fdcbba165f8d88c6498a5965
shant-k <shantk650@gmail.com>
2023-05-05 10:29:00 +0300

    protected fav and items

--- routes/favorite.js
+++ routes/favorite.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const upload= require('../middleware/upload')
+const { protect } = require("../middleware/authMiddleware");
 
 const { getfavitems,getfavbyid,  postfavitems,deletefavitems,updatefavitems }=require("../controllers/favorites");
 
 router.get("/getfav", getfavitems)
 router.get("/getfav/:id", getfavbyid );
-router.post("/addfav",upload.single('image_fav'), postfavitems)
-router.delete("/delfav/:id", deletefavitems )
-router.put("/updfav/:id", updatefavitems )
+router.post("/addfav", protect,upload.single('image_fav'), postfavitems)
+router.delete("/delfav/:id",protect, deletefavitems )
+router.put("/updfav/:id",protect, updatefavitems )
 
 
 
